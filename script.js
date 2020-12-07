de766a8a51db76937b9a128c07e9335178c3f0c4
Yuri Weilemann <t2m.yweilemann@terceiros.mag.com.br>
2020-12-07 20:28:09 -0300

    Changing game time to 1000 seconds (kids need a help)

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ if (document.readyState == 'loading') {
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(100, cards);
+    let game = new MixOrMatch(1000, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
